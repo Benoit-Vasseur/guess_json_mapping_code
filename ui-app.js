4a3a84fa6bd66ebad3cf1c5fe1a37b4b76d04d00
Benoit Vasseur <benoit.vasseur14@gmail.com>
2019-02-03 19:03:03 -0500

    add jsoneditor for inputs

--- ui-app.js
+++ ui-app.js
@@ -1,29 +1,45 @@
 ;(function bootstrap() {
   document.addEventListener("DOMContentLoaded", () => {
-    setupGenerateButtonListener()
+    // create the editors
+    var sourceContainer = document.querySelector("#source-jsoneditor")
+    var options = { mode: "code" }
+    const source = {
+      p: {
+        firstname: "Benoit"
+      },
+      lastname: "Vasseur"
+    }
+    var sourceEditor = new JSONEditor(sourceContainer, options, source)
+
+    var targetContainer = document.querySelector("#target-jsoneditor")
+    const target = {
+      personne: {
+        firstName: "Benoit",
+        lastName: "Vasseur"
+      }
+    }
+    var targetEditor = new JSONEditor(targetContainer, options, target)
+
+    setupGenerateButtonListener(sourceEditor, targetEditor)
   })
 
-  function setupGenerateButtonListener() {
+  function setupGenerateButtonListener(sourceEditor, targetEditor) {
     document
       .querySelector("#generate-button")
-      .addEventListener("click", function() {
-        const mappingRules = window.guessMapingRules(
-          JSON.parse(getSourceInput().value),
-          JSON.parse(getTargetInput().value)
-        )
-        const formatedMappingRules = JSON.stringify(mappingRules, null, 4)
-        const sourceCode = window.generateMappingCode(formatedMappingRules)
-        getSourceCodeNode().textContent = sourceCode
-        window.Prism.highlightElement(getSourceCodeNode())
+      .addEventListener("click", function clickOnGenerate() {
+        generateSourceCode(sourceEditor, targetEditor)
       })
   }
 
-  function getSourceInput() {
-    return document.querySelector("#source-input")
-  }
-
-  function getTargetInput() {
-    return document.querySelector("#target-input")
+  function generateSourceCode(sourceEditor, targetEditor) {
+    const mappingRules = window.guessMapingRules(
+      sourceEditor.get(),
+      targetEditor.get()
+    )
+    const formatedMappingRules = JSON.stringify(mappingRules, null, 4)
+    const sourceCode = window.generateMappingCode(formatedMappingRules)
+    getSourceCodeNode().textContent = sourceCode
+    window.Prism.highlightElement(getSourceCodeNode())
   }
 
   function getSourceCodeNode() {
